2fec82ea6d5ae66493f40ea303e8f0bd7db21993
beizi1 <88990181+beizi1@users.noreply.github.com>
2021-08-17 15:40:25 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -2,23 +2,41 @@
 #week 3 assignment 2
 
 makeCacheMatrix <- function(x=matrix()){
-        invers=NULL
+        #invers - inverse matrix value
+        invers=NULL               #default value - NULL
+        
+        #go function: matrix and reset invers
         go <- function(y){
                 x<<-y
                 invers<<-NULL
         }
+        #triv function: get matrix
         triv <- function(){x}
+        
+        #goInvers function: set inverse matrix
         goInverse <- function(inverse){invers <<- inverse}
+        
+        #trivInvers function: get inverse matrix
         trivInverse <- function(){invers}
         list(go=go,triv=triv,goInverse=goInverse, trivInverse=trivInverse)
 }
 
+
+
+## Description: This function computes the inverse of the
+## 				special "matrix" returned by makeCacheMatrix
 cacheSolve <- function(x, ...){
+        #get invers Matrix
         invers <- x$trivInverse()
+        
+        #check if it is not null
         if(!is.null(invers)){
                 message("Return cache data")
                 return(invers)
         }
+        
+        ## Return a matrix that is the inverse of 'x'
+        #if it is null, calculate the inverse
         mat <- x$triv()
         invers <- solve(mat, ...)
         x$goInverse(invers)
